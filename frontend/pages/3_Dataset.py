c0d338ccc29cc5acf3b0c2cb1389a15b2640f222
Hassanibrar632 <hassanibrar632@gmail.com>
2024-10-23 19:15:38 +0500

    Update 3_Dataset.py

--- frontend/pages/3_Dataset.py
+++ frontend/pages/3_Dataset.py
@@ -23,59 +23,67 @@ def login_message():
         st.write_stream(response_generator())
 
 def data_upload():
-    st.set_page_config(page_title="Dataset Dashboard", page_icon="📊")
     ##########################
     #  Get dataset of users  #
     ##########################
-    st.title("Select dataset from your history")
+    Datasets = {}
     # create a list of dataset from previous uploads
 
     st.title("Upload Your CSV Dataset")
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     data_type = st.select_slider("Select the type of the Dataset", options=['Regression', 'Classification'])
     if uploaded_file is not None:
-        name = uploaded_file.name
         df = pd.read_csv(uploaded_file)
-        data_type = data_type
-        user = st.session_state.user
-        #########################
-        # call API to save data #
-        #########################
-        
-        st.success("File uploaded successfully!")
-        st.rerun()
+        cols = df.columns
+    if cols:
+        target = st.selectbox("Select the Target column", cols)
+
+    if st.button('Submit Dataset'):
+        if uploaded_file is not None:
+            payload = {
+                "name": uploaded_file.name,
+                "df": pd.read_csv(uploaded_file),
+                "data_type": data_type,
+                "user": st.session_state.user,
+                "target": target,
+            }
+            #########################
+            # call API to save data #
+            #########################
+            
+            st.success("File uploaded successfully!")
+            st.rerun()
 
     else:
         st.info("Please upload a CSV file to proceed.")
 
-def data_visulization():
+def data_manager():
     st.set_page_config(page_title="Dataset", page_icon="📊")
-    st.title("Dataset Dashboard"):
-    ########################################
-    # Tab to dataset info for both dataset #
-    ########################################
-    # select the target column first
+    st.title("Dataset Dashboard")
+    tabs = st.tabs(['Previous Datasets', 'Upload Dataset', 'Edit Dataset'])
+    with tabs[0]:
+        #########################
+        # Tab to select dataset #
+        #########################
+        pass
 
-    ##########################
-    # Tab to orignal dataset #
-    ##########################
-
-    #########################
-    # Tab to edited dataset #
-    #########################
-    # select columns to display
-    # perform operation on selected columns
-    return
-
-if st.session_state.logged:
-    if st.session_state.df == None:
+    with tabs[1]:
         data_upload()
-    else:
-        data_visulization()
-else:
-    login_message()
+        pass
 
+    with tabs[2]:
+        #########################
+        # Tab to edited dataset #
+        #########################
+        pass
+
+    return
 
 if 'Logged' not in st.session_state:
     st.session_state.Logged = False
     st.session_state.user = None
+
+if st.session_state.logged:
+    data_manager()
+else:
+    login_message()
